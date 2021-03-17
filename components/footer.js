accb1dafa6218de79c1bda91b6427cc2fee63836
asmenezes <asmenezes23@gmail.com>
2021-03-17 12:17:49 -0400

    Added Socials to outer footer

--- components/footer.js
+++ components/footer.js
@@ -6,9 +6,15 @@ class Footer extends HTMLElement {
     this.innerHTML = `
     <footer>
   <h1 class="fquote">Secure your home from mother nature and the power bill</h1>
-  <div class="fsocials">
 
+  <div class="fsocials">
+  <span></span>
+  <a href=""> <img src="./assets/InstaLogo.png" class = "soc" alt="instagram"/> </a>
+  <a href=""> <img src="./assets/yelp-icon.png" class = "soc" alt="yelp"/> </a>
+  <a href=""> <img src="./assets/Nextdoor.png" class = "soc" alt="Nextdoor"/> </a>
+  <span></span>
   </div>
+
   <br>
   <p class="ftext">info@tarheelroofingandsolar.com</p>
   <br>
