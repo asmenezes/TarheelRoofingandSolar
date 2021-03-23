23358c1426d9db67a130a33d26ec2431dfd34389
asmenezes <asmenezes23@gmail.com>
2021-03-22 21:05:31 -0400

    "Under COnstruction"

--- components/baseheader.js
+++ components/baseheader.js
@@ -15,26 +15,26 @@ class BaseHeader extends HTMLElement {
       <li onclick="menuSwap()"> <img src="./assets/menu.svg" alt="menu" class="menu"> </li>
     <li><a href="./index.html">HOME</a></li>
     <li><div class="navdropdown">
-      <a href="./about/what-we-do.html">
+      <a href="./under-construction.html">
       ABOUT
       </a>
       <div class="navdropdowncont">
-        <a href="./about/owners.html">OWNERS</a>
-        <a href="./about/business.html">BUSINESS PHILOSOPHY</a>
-        <a href="./about/integrity.html">INTEGRITY</a>
+        <a href="./under-construction.html">OWNERS</a>
+        <a href="./under-construction.html">BUSINESS PHILOSOPHY</a>
+        <a href="./under-construction.html">INTEGRITY</a>
       </div>
       </div></li>
       <li><div class="navdropdown">
-        <a href="./operations/operations.html">
+        <a href="./under-construction.html">
         OPERATIONS
         </a>
         <div class="navdropdowncont">
-          <a href="./operations/asphalt.html">ASPHALT ROOFING</a>
-          <a href="./operations/metal.html">METAL ROOFING</a>
-          <a href="./operations/solar.html">SOLAR</a>
-          <a href="./operations/siding.html">SIDING</a>
-          <a href="./operations/gutters.html">GUTTERS</a>
-          <a href="./operations/painting.html">INTERIOR/EXTERIOR PAINTING</a>
+          <a href="./under-construction.html">ASPHALT ROOFING</a>
+          <a href="./under-construction.html">METAL ROOFING</a>
+          <a href="./under-construction.html">SOLAR</a>
+          <a href="./under-construction.html">SIDING</a>
+          <a href="./under-construction.html">GUTTERS</a>
+          <a href="./under-construction.html">INTERIOR/EXTERIOR PAINTING</a>
         </div>
         </div></li>
       <li><a href="./testimonials.html"> TESTIMONIALS</a></li>
@@ -43,10 +43,10 @@ class BaseHeader extends HTMLElement {
         CONTACT US
         </a>
         <div class="navdropdowncont">
-          <a href="./careers.html">CAREERS</a>
+          <a href="./under-construction.html">CAREERS</a>
         </div>
         </div></li>
-        <li><a href="./testimonials.html" class="circled"> ASAP-FIX</a></li>
+        <li><a href="./under-construction.html" class="circled"> ASAP-FIX</a></li>
   </ul>
   </nav>
 
